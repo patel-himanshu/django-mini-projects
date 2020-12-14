1dc61fb0ee68fffe4336615a206f13522f7e1cbe
Himanshu Patel <hpatel27299@gmail.com>
2020-12-14 23:38:45 +0530

    Blog Site (Added models, views, URLs)

---
+++ blog_site/blog/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+# Create your models here.
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset().filter(status='published')
+
+class Post(models.Model):
+    STATUS_CHOICES = (
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+    )
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique_for_date='publish')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+
+    objects = models.Manager() # Default manager
+    published = PublishedManager() # Custom manager
+    
+    class Meta:
+        ordering = ('-publish',)
+        db_table = 'blog_post_tables' # Default table name = 'blog_post'
+
+    def __str__(self):
+        return self.title
