33b180960c2e7aeb4648e7dd23d64853c88db64d
Himanshu Patel <hpatel27299@gmail.com>
2020-12-16 02:32:39 +0530

    Blog Site (Added pagination, class-based view)

--- blog_site/blog/views.py
+++ blog_site/blog/views.py
@@ -1,10 +1,31 @@
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView
 from .models import Post
 
 # Create your views here.
+""" Function-based View of Post List
 def post_list(request):
     posts = Post.published.all()
-    return render(request, 'blog/post/list.html', {'posts': posts})
+    paginator = Paginator(posts, 5)
+    curr_page = request.GET.get('page') # Used to retrieve the current page number
+    
+    try:
+        posts = paginator.page(curr_page) # Would return the results of current page
+    except PageNotAnInteger:
+        posts = paginator.page(1) # Would return the first page of results
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages) # Would return the last page of results
+    
+    return render(request, 'blog/post/list.html', {'posts': posts, 'page': curr_page})
+"""
+
+# Class-based View of Post List
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 5
+    template_name = 'blog/post/list.html'
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(
