e5950bcef46c129ee6051f75b85974853759927c
Himanshu Patel <hpatel27299@gmail.com>
2020-12-19 23:52:15 +0530

    Blog Site (Added share using e-mail option)

--- blog_site/blog/views.py
+++ blog_site/blog/views.py
@@ -1,6 +1,8 @@
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
+from django.core.mail import send_mail
+from .forms import EmailPostForm
 from .models import Post
 
 # Create your views here.
@@ -37,3 +39,24 @@ def post_detail(request, year, month, day, post):
         publish__day = day
     )
     return render(request, 'blog/post/detail.html', {'post': post})
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False
+
+    if request.method == 'POST':
+        # A filled form is submitted, which needs to be processed
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # Sends an e-mail, if the form is valid
+            # If the form is invalid, then the template is loaded again using render()
+            cd = form.cleaned_data # Creates a dictionary of form fields and its values 
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read {post.title}"
+            message = f"Read {post.title} at {post_url}\n\n{cd['name']}\'s comments: {cd['comments']}"
+            send_mail(subject, message, 'somedummyid@gmail.com', [cd['to']])
+            sent = True
+    else:
+        # Displays a new instance of an empty form
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
