ca5ea45662cbc94b0374a53cd35aac789b519e00
Himanshu Patel <hpatel27299@gmail.com>
2021-01-15 12:46:16 +0530

    Blog Site - Added RSS Feed

--- blog_site/blog/urls.py
+++ blog_site/blog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+from .feeds import LatestPostsFeed
+
 app_name = 'blog'
 
 urlpatterns = [
@@ -9,4 +11,5 @@ urlpatterns = [
     path('tag/<slug:tag_slug>/', views.post_list, name='post_list_by_tag'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
     path('<int:post_id>/share/', views.post_share, name='post_share'),
+    path('feed/', LatestPostsFeed(), name='post_feed'),
 ]
