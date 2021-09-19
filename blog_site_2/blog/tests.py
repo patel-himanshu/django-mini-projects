a7e0bfd93ed3e495bd99d6bb83067ca424052e00
Himanshu Patel <44549418+patel-himanshu@users.noreply.github.com>
2021-09-20 00:30:49 +0530

    test: [Blog 2] Add test for post (list and detail) view

--- blog_site_2/blog/tests.py
+++ blog_site_2/blog/tests.py
@@ -1,3 +1,49 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
+from django.urls import reverse
+
+from .models import Post
 
 # Create your tests here.
+
+
+class BlogTest(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username="testuser",
+            email="test@gmail.com",
+            password="somesecret"
+        )
+
+        self.post = Post.objects.create(
+            title="A test blog",
+            author=self.user,
+            body="Some gibberish test content for the blog"
+        )
+
+    def test_string_representation(self):
+        post = Post(title="A test blog")
+        self.assertEqual(str(post), post.title)
+
+    def test_post_content(self):
+        self.assertEqual(f"{self.post.title}", "A test blog")
+        self.assertEqual(f"{self.post.author}", "testuser")
+        self.assertEqual(f"{self.post.body}",
+                         "Some gibberish test content for the blog")
+
+    def test_post_list_view(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(
+            response, "Some gibberish test content for the blog")
+        self.assertTemplateUsed(response, "home.html")
+
+    def test_post_detail_view(self):
+        response = self.client.get("/post/1/")
+        no_response = self.client.get("/post/100/")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(no_response.status_code, 404)
+        self.assertContains(response, "A test blog")
+        self.assertContains(
+            response, "Some gibberish test content for the blog")
+        self.assertTemplateUsed(response, "post_detail.html")
