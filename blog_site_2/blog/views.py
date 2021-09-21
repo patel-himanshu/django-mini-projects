9fd264a46b2d529d34449b693aeae7e73596442e
Himanshu Patel <44549418+patel-himanshu@users.noreply.github.com>
2021-09-21 14:49:33 +0530

    feat: [Blog 2] Update and delete blogs

--- blog_site_2/blog/views.py
+++ blog_site_2/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import DetailView, ListView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from django.urls import reverse_lazy
 
 from .models import Post
 
@@ -21,3 +22,15 @@ class BlogCreateView(CreateView):
     model = Post
     template_name = 'post_new.html'
     fields = ['title', 'author', 'body']
+
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title', 'body']
+
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
