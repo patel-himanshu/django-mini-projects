a83cb9123b659806da280ac769112ee8b787c266
Himanshu Patel <44549418+patel-himanshu@users.noreply.github.com>
2021-09-23 23:15:57 +0530

    refactor: [Newspaper] Home page view

--- newspaper/config/urls.py
+++ newspaper/config/urls.py
@@ -15,11 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic.base import TemplateView
+
+# from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('django.contrib.auth.urls')),
     path('account/', include('account.urls')),
-    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    # path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('', include('page.urls')),
 ]
