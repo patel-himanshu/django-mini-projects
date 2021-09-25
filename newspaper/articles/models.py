b92e526048ac4ca9f1a113c24e6642b6de3def30
Himanshu Patel <44549418+patel-himanshu@users.noreply.github.com>
2021-09-26 02:12:37 +0530

    feat: [Newspaper] Add comments for articles

--- newspaper/articles/models.py
+++ newspaper/articles/models.py
@@ -16,3 +16,16 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Comment(models.Model):
+    article = models.ForeignKey(
+        Article, on_delete=models.CASCADE, related_name='comments')
+    comment = models.CharField(max_length=150)
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+
+    def get_absolute_url(self):
+        return reverse('article_list')
+
+    def __str__(self):
+        return self.comment[:50]
