b74f1c17005dcb982df7a84c4668654a1a25d1f8
Himanshu Patel <44549418+patel-himanshu@users.noreply.github.com>
2021-09-27 23:51:15 +0530

    feat: [Library] Create books app

---
+++ library_site/book/views.py
@@ -0,0 +1,10 @@
+from django.views.generic import ListView
+
+from .models import Book
+
+# Create your views here.
+
+
+class BookListView(ListView):
+    model = Book
+    template_name = 'book_list.html'
