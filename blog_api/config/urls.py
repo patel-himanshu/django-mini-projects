c59d7e136eafb6a747ac06bc4500175aac48a8a0
Himanshu Patel <44549418+patel-himanshu@users.noreply.github.com>
2021-09-29 11:16:53 +0530

    feat: [Blog API] Create APIs for CRUD on blog

--- blog_api/config/urls.py
+++ blog_api/config/urls.py
@@ -18,5 +18,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('api/', include('post.urls')),
+    path('api/v1/', include('post.urls')),
 ]
---
+++ blog_api/post/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from .views import PostDetail, PostList
+
+urlpatterns = [
+    path('', PostList.as_view()),
+    path('<int:pk>/', PostDetail.as_view()),
+]
